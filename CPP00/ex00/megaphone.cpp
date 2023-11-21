60b4be4f8bebb5521390459525cb40cd0af99c2b
Nisrin Boukhari <niboukha@e2r11p10.1337.ma>
2023-11-21 10:55:02 +0100

    check

--- CPP00/ex00/megaphone.cpp
+++ CPP00/ex00/megaphone.cpp
@@ -6,28 +6,31 @@
 /*   By: niboukha <niboukha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/16 15:13:23 by niboukha          #+#    #+#             */
-/*   Updated: 2023/11/20 13:15:52 by niboukha         ###   ########.fr       */
+/*   Updated: 2023/11/21 10:51:54 by niboukha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <string.h>
 
 int	main(int ac, char **av)
 {
+	std::string	arg;
+	std::size_t	j;
 	int	i;
-	int	j;
 
-	if (ac > 1)
+	if (ac == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	else
 	{
 		i = 0;
 		while (av[++i])
 		{
+			arg = av[i];
 			j = -1;
-			while (av[i][++j])
-				std::cout << (char)std::toupper(av[i][j]);
+			while (++j < arg.length())
+				std::cout << (char)std::toupper(arg[j]);
 		}
 	}
-	else
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
 	return (0);
 }
